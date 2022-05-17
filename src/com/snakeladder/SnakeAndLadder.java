b388ebaa7de9f410cc0b5a3514cde608c23c0053
Charsh177 <harshchougule177@gmail.com>
2022-05-17 08:14:51 +0530

    ADDED: Repeat till player reaches winning position

--- src/com/snakeladder/SnakeAndLadder.java
+++ src/com/snakeladder/SnakeAndLadder.java
@@ -3,20 +3,36 @@ package com.snakeladder;
 public class SnakeAndLadder
 {
     public static void main(String[] args) {
-        int option = (int) (Math.random() * 4);
+        int startPosition = 0;
+        System.out.println("Starting position of player is " + startPosition);
+        final int NO_PLAY = 0;
+        final int IS_LADDER = 1;
+        final int IS_SNAKE = 2;
+        int position = 0;
 
-        switch (option)
+        while (position < 100) {
+            int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+            System.out.println("Dice number is " + diceNumber);
 
-        {
-            case 1:
-                System.out.println("No play: Player in same position");
-                break;
-            case 2:
-                System.out.println("Ladder: Player moves ahead");
-                break;
-            case 3:
-                System.out.println("Snake: Player moves behind");
+            int option = (int) (Math.floor(Math.random() * 10) % 3);
+            System.out.println("Player option is " + option);
+
+            switch (option) {
+                case 1:
+                    position = diceNumber + position;
+                    System.out.println("Position of the player after ladder is " + position);
+                    break;
+                case 2:
+                    position = position - diceNumber;
+                    if (position < 0) {
+                        position = 0;
+                    }
+                    System.out.println("Position of the player after snake is " + position);
+                    break;
+                default:
+                    System.out.println("No play");
+            }
+
         }
-
     }
 }
