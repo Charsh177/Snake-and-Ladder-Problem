02114d50fd7004e6ff57eb40f1f969ea533d58be
Charsh177 <harshchougule177@gmail.com>
2022-05-17 07:43:01 +0530

    ADDED: Playes starts from start position

--- src/com/snakeladder/SnakeAndLadder.java
+++ src/com/snakeladder/SnakeAndLadder.java
@@ -4,6 +4,8 @@ public class SnakeAndLadder {
 
     public static void main(String[] args)
     {
+        int initialPosition=0;
         System.out.println("Welcome to Snake and Ladder Game Program");
+        System.out.println("You are in "+ initialPosition +" position");
     }
 }
