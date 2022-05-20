f1df116bdeef0c9c2f5ee6559e82304670e531f0
Charsh177 <harshchougule177@gmail.com>
2022-05-20 19:12:55 +0530

    ADDED: Report the number of times dice was played to win

--- src/com/snakeladder/SnakeAndLadder.java
+++ src/com/snakeladder/SnakeAndLadder.java
@@ -2,35 +2,48 @@ package com.snakeladder;
 
 public class SnakeAndLadder {
 
+    public static final int WINNING_POSITION = 100;
+    public static final int IS_LADDER = 1;
+    public static final int IS_SNAKE = 0;
+
     public static void main(String[] args) {
 
-        int NO_PLAY = 0;
-        int IS_LADDER = 1;
-        int IS_SNAKE = 2;
-        int position = 0;
+        System.out.println("Welcome to Snake And Ladder Program");
+        int startPosition = 0;
+        System.out.println("Starting position of player is " + startPosition);
 
-        while(position < 100) {
-            int diceNumber = (int)(Math.floor(Math.random() * 10) % 6 + 1);
+        int position = 0;
+        int diceCount = 0;
+        while (position < WINNING_POSITION) {
+            ++diceCount;
+
+            int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
             System.out.println("Dice number is " + diceNumber);
 
-            int option = (int)(Math.floor(Math.random() * 10) % 3);
+            int option = (int) (Math.floor(Math.random() * 10) % 2);
             System.out.println("Player option is " + option);
 
-            switch(option) {
-                case 1:
-                    position = diceNumber + position;
+            switch (option) {
+                case IS_SNAKE: {
+                    System.out.println("Snake for the player");
+                    position = position - diceNumber;
+                    if (position - diceNumber < 0)
+                        position = 0;
+                    System.out.println("Position of the player after snake is " + position);
+                    break;
+                }
+                case IS_LADDER: {
+                    System.out.println("Ladder for the player");
+                    if (position + diceNumber > 100)
+                        position = position + diceNumber;
                     System.out.println("Position of the player after ladder is " + position);
                     break;
-                case 2:
-                    if (diceNumber + position <= 100) {
-                        position = position + diceNumber;
-                        System.out.println("Position of the player is " + position);
-                        break;
-                    }
+                }
                 default:
                     System.out.println("No play");
+                    break;
             }
-
         }
+        System.out.println("Total dice roll count is " + diceCount);
     }
 }
